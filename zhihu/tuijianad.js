f5c5903c3b35e86cf3fe9cc17737e54e0c704d38
xiakong24 <35594893+xiakong24@users.noreply.github.com>
2020-08-18 23:36:33 +0800

    Update tuijianad.js

--- zhihu/tuijianad.js
+++ zhihu/tuijianad.js
@@ -1,1 +1,9 @@
-
+var body = $response.body
+var obj=JSON.parse(body)
+obj['data'].forEach((element, index)=> {
+    if(element['card_type']=='slot_event_card'||element.hasOwnProperty('ad')){      
+       obj['data'].splice(index,1)  
+    }
+})
+body=JSON.stringify(obj)
+$done({body})
