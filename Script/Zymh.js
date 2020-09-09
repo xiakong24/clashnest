6a9717c5735e2ac2728cfffc734ba74272cc7055
xiakong24 <35594893+xiakong24@users.noreply.github.com>
2020-09-09 16:32:44 +0800

    Update Zymh.js

--- Script/Zymh.js
+++ Script/Zymh.js
@@ -36,7 +36,10 @@ else if (url.indexOf(ticket) != -1) {
 else if (url.indexOf(free) != -1) {
 	body.data.price = 0;
 	body.data.download_price = 0;
-for (var i=0;i<body.data.chapter_list.length;i++) {body.data.chapter_list[i].price=0;body.data.chapter_list[i].download_price=0;}
+for (var i=0;i<body.data.chapter_list.length;i++) {
+	body.data.chapter_list[i].price=0;
+	body.data.chapter_list[i].download_price=0;
+};
 }
 
 	body = JSON.stringify(body);
