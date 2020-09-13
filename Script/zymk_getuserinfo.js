1d51d7cf4d41c59efab24b8239f6268e06620d2c
xiakong24 <35594893+xiakong24@users.noreply.github.com>
2020-09-13 10:37:43 +0800

    Create zymk_getuserinfo.js

---
+++ Script/zymk_getuserinfo.js
@@ -0,0 +1,8 @@
+let body = JSON.parse($response.body);
+body.data.coins = 6666;
+body.data.isvip = 1;
+body.data.vipdays = 9999;
+body.data.recommend = 6666;
+body.data.Cticket = 6666;
+body.data.Cgold = 6666;
+$done({body});
